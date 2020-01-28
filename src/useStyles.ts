d3fc131743e0bb2b3cce4ea59e5d56a295c18160
Sutan Nasution <sutan.gnst@gmail.com>
2020-01-28 09:21:20 +0700

    merged from Brettm12345-csstype

--- src/useStyles.ts
+++ src/useStyles.ts
@@ -1,5 +1,5 @@
-import * as CSS from "csstype";
+import { Properties } from 'csstype'
 
 export const useStyles = <K extends string = string>(
-  styles: Record<K, CSS.Properties>
-): Record<K, CSS.Properties> => styles;
+  styles: Record<K, Properties>
+): Record<K, Properties> => styles
